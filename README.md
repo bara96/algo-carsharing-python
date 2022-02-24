b8c31a03fd94a4b8addce349836aa54c0a63dcb5
Matteo Baratella <mbaratella@walit.it>
2022-02-24 16:50:57 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# algo-carsharing
-Smart Contract deveoped in PyTeal to build a dApp for car sharing in Algorand Blockchain
+# Algo Carsharing
+Smart Contract developed in [PyTeal](https://developer.algorand.org/docs/get-details/dapps/pyteal/) to build a dApp for car sharing in Algorand Blockchain
+
+
+# Requirements
+- [Python3.6 or higher](https://www.python.org/downloads/)
+- [Docker](https://www.docker.com/products/docker-desktop)
+- [Docker Compose](https://docs.docker.com/compose/)
+
+## Sandbox environment
+In order to deploy any transaction on Algorand Blockchain you need first of all setup an Algorand node.  
+You can easily setup an Algorand **sandbox** node following the [official guide](https://github.com/algorand/sandbox#algorand-sandbox) or following [this](https://developer.algorand.org/docs/get-started/dapps/pyteal/#install-sandbox) tutorial.  
+
+We'll use the **Testnet** sandbox:  
+<code>./sandbox start testnet</code>
+
+## Python environment and dependencies
+Install the required dependencies for the project.  
+Using a Python virtual environment (**venv**) is recommended to do this.  
+The following commands will activate the virtual environment and then install all dependencies, including PyTeal and the Algorand Python SDK.
+
+Setup Python environment (one time).  
+<code>python3 -m venv venv</code>  
+
+Activate venv. Replace bin with Scripts on Windows.  
+<code>. venv/bin/activate</code>
+
+Make sure that you have pip installed.
+If not, you can install it like this:  
+<code>sudo apt install python3-pip</code>  
+
+Now install all requirements.  
+<code>pip3 install -r requirements.txt</code>
+
+
---
+++ account.py
@@ -0,0 +1,99 @@
+import base64
+import json
+import os
+
+from algosdk import account, mnemonic
+from algosdk import constants
+from algosdk.future import transaction
+from algosdk.v2client import algod
+from dotenv import load_dotenv
+
+import utils as ut
+
+
+def generate_algorand_keypair():
+    ut.console_log("Generating keypair..", "green")
+    private_key, address = account.generate_account()
+    passphrase = mnemonic.from_private_key(private_key)
+    print("New address: {}".format(address))
+    print("New private key: {}".format(private_key))
+    print("New passphrase: {}".format(passphrase))
+    ut.console_log("Save values into .env", "yellow")
+
+
+def read_algorand_keypair():
+    ut.console_log("Reading keypair..", "green")
+    load_dotenv()
+    address = os.getenv('ADDRESS')
+    private_key = os.getenv('PRIVATE_KEY')
+    passphrase = mnemonic.from_private_key(private_key)
+
+    print("Address: {}".format(address))
+    print("Private key: {}".format(private_key))
+    print("Passphrase: {}".format(passphrase))
+    return address, private_key
+
+
+def test_transaction(private_key, my_address):
+    # default host and token for testnet sandbox
+    algod_address = "http://localhost:4001"
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+
+    print("My address: {}".format(my_address))
+    account_info = algod_client.account_info(my_address)
+    print("Account balance: {} microAlgos".format(account_info.get('amount')))
+
+    # build transaction
+    params = algod_client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = constants.MIN_TXN_FEE
+    params.fee = 1000
+    receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA"
+    note = "Hello World".encode()
+
+    unsigned_txn = transaction.PaymentTxn(my_address, params, receiver, 1000000, None, note)
+
+    # sign transaction
+    signed_txn = unsigned_txn.sign(private_key)
+
+    # submit transaction
+    txid = algod_client.send_transaction(signed_txn)
+    print("Signed transaction with txID: {}".format(txid))
+
+    # wait for confirmation
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
+    except Exception as err:
+        print(err)
+        return
+
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+    print("Decoded note: {}".format(base64.b64decode(
+        confirmed_txn["txn"]["txn"]["note"]).decode()))
+
+    print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
+    print("Amount transfered: {} microAlgos".format(1) )
+    print("Fee: {} microAlgos".format(params.fee) )
+
+
+    account_info = algod_client.account_info(my_address)
+    print("Final Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('What do you want to do?')
+    print('1) Generate keypair')
+    print('2) Read existing keypair')
+    print('3) Perform a transaction')
+    x = int(input())
+    if x == 1:
+        generate_algorand_keypair()
+    elif x == 2:
+        read_algorand_keypair()
+    elif x == 3:
+        address, private_key = read_algorand_keypair()
+        test_transaction(private_key, address)
+    else:
+        print("Unknown action.")
