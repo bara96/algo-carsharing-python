a0200c90a3b96a6d4171d5f606928c222b10e110
Matteo Baratella <matteo.baratella96@gmail.com>
2022-03-29 18:08:50 +0200

    Updated contract to support escrow (to test)

--- smart_contracts/contract_escrow.py
+++ smart_contracts/contract_escrow.py
@@ -10,8 +10,8 @@ class EscrowContract:
         return Seq([
             Assert(Global.group_size() == Int(2)),
             Assert(Gtxn[0].application_id() == Int(app_id)),
-            Assert(Gtxn[0].approval_program() == self.Constants.approval_program_hash),
-            Assert(Gtxn[0].clear_state_program() == self.Constants.clear_state_program_hash),
+            # Assert(Gtxn[0].approval_program() == self.Constants.approval_program_hash),
+            # Assert(Gtxn[0].clear_state_program() == self.Constants.clear_state_program_hash),
 
             Assert(Gtxn[1].type_enum() == TxnType.Payment),
 
